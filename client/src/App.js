079dec84867823138d538bf4eed68cb8ec487807
Rohill <rohillnand1@gmail.com>
2020-06-04 20:16:27 +1200

    react nav bar, container created

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import AppNavbar from './components/AppNavbar';
+import { Container, Row, Col, Button } from 'reactstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppNavbar />
+      <Container fluid>
+        <Row>
+          <Col className="text-center mt-5"><h1>Welcome Page</h1>
+            <Button variant="contained" color="primary" onClick={console.log("test")}>
+              Click Me
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
