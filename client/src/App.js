0e9704bf692b3ebdd0e38fce239733266acb4cca
Rohill <rohillnand1@gmail.com>
2020-06-05 19:29:24 +1200

    able to fetch get method from first application and display country names

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import AppNavbar from './components/AppNavbar';
+import Countries from './components/Countries';
 import { Container, Row, Col, Button } from 'reactstrap';
 
 function App() {
+  const [countries, getCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("/getcountries").then(res => 
+      res.json().then(data => {
+        getCountries(data);
+      })
+    );
+  }, []);
+
   return (
     <div>
       <AppNavbar />
@@ -13,6 +24,7 @@ function App() {
             <Button variant="contained" color="primary" onClick={console.log("test")}>
               Click Me
             </Button>
+            <Countries countries={countries}/>
           </Col>
         </Row>
       </Container>
